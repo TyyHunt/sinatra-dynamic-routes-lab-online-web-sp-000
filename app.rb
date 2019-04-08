f6cba2f65c946b969bfe2a0674e211daa0053457
Tyler Hunt <tyhunt04@gmail.com>
2019-04-08 22:13:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,4 +6,10 @@ class App < Sinatra::Base
     @name = params[:name]
     "#{@name.reverse}"
   end
+
+  get '/square/:number' do
+    @num = params[:number].to_i
+    @squared = @num * @num
+    "#{@squared}"
+  end
 end
